07cc1f58bca4b8fc085ea518129bfaca05f1b70f
Maruf Mobin <mobinh264@gmail.com>
2021-12-14 20:12:17 +0600

    contact page work done

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -3,20 +3,14 @@ import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
-// import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
-// import InboxIcon from '@mui/icons-material/MoveToInbox';
-// import List from '@mui/material/List';
-// import ListItem from '@mui/material/ListItem';
-// import ListItemIcon from '@mui/material/ListItemIcon';
-// import ListItemText from '@mui/material/ListItemText';
-// import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { Link, useRouteMatch } from 'react-router-dom';
 import Menu from '../Menu/Menu';
+import './Contact.css'
 
 const drawerWidth = 240;
 
@@ -107,12 +101,16 @@ function Contact(props) {
                 sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
             >
                 <Toolbar />
-                <Typography paragraph>
-                   Contact 
-                </Typography>
-                <Typography paragraph>
-                    Contact
-                </Typography>
+                <form className='main-form'>
+                    <h2 style={{textAlign: "left", color:"#fff",fontSize:"42px", fontWeight:"400"}}>Contact Me</h2>
+                    <p style={{textAlign:"left", fontSize:"22px", color:"#fff", marginBottom:"10px"}}>Fast Name</p>
+                    <input type="text" className='form-control' />
+                    <p style={{textAlign:"left", fontSize:"22px", color:"#fff", marginBottom:"10px"}}>Last Name </p>
+                    <input type="text" className='form-control'/>
+                    <p style={{textAlign:"left", fontSize:"22px", color:"#fff", marginBottom:"10px"}}>Email</p>
+                    <input type="email" className='form-control'/>
+                    <div style={{textAlign:"left"}}><input type="submit" className='form-btn' /></div>
+                </form>
             </Box>
         </Box>
     );
